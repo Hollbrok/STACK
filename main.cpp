4d8e16cdf799fc408b7bebbdef99c12801a80746
Talashkevich Daniil <70323332+Hollbrok@users.noreply.github.com>
2020-11-02 01:38:12 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,31 +1,13 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <math.h>
-#include <assert.h>
-#include <stdbool.h>
-#include <iostream>
-
-struct node
-{
-    double data;
-    struct node* p;
-};
-
-void push(node **pptop, double date);
-double pop(node **pptop);
-void print(node **pptop);
+#include "stack.h"
 
 int main()
 {
-    struct node *ptop = NULL;
+    FILE* res = fopen("log.txt", "wb");
+    node *ptop = NULL;
 
     push(&ptop, 40);
     push(&ptop, 0.012);
-    push(&ptop, 222/0);
-
-
-
+    //push(&ptop, 222/0);
 
     print(&ptop);
     print(&ptop);
@@ -39,74 +21,8 @@ int main()
     print(&ptop);
     //printf("%.2lf \n", pop(&ptop));
 
+
+    fclose(res);
+
     return 0;
-}
-
-double pop(node **pptop)
-{
-    struct node *temp;
-    double x;
-
-    if(*pptop == NULL)
-    {
-        printf("STACK IS EMPTY");
-        exit(0);
-    }
-
-    temp = *pptop;
-    x = temp->data;
-
-
-
-    *pptop = temp -> p;
-    free(temp);
-
-    /*if(x == 0)
-    {
-        return 0;
-    }*/
-
-    if(x == EOF)
-    {
-        printf("Trying of hacking stack if failed, returned ");
-        return 0;
-    }
-
-    /*if(isnan(x))
-    {
-        printf("Trying of hacking stack if failed, returned ");
-        return 0;
-    }*/
-
-    if(fpclassify (x) != FP_NORMAL)
-    {
-        printf("Trying of hacking stack if failed, returned ");
-        return 0;
-    }
-
-    return x;
-}
-
-void push(node **pptop,double date)
-{
-
-
-
-    node *temp;
-    temp = (struct node*) calloc(1, sizeof( node));
-    if(temp == NULL)
-    {
-        printf("STACK IS FULL");
-        exit(0);
-    }
-
-    temp->data = date;
-    temp->p    = *pptop;
-    *pptop     = temp;
-
-}
-
-void print(node **pptop)
-{
-    printf("%.2lf \n", pop(pptop));
 }
