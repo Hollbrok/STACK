9ca04f53068c2b28ae14425869ead06a40f67513
Talashkevich Daniil <70323332+Hollbrok@users.noreply.github.com>
2020-11-09 11:58:51 +0300

    Update stack.h

--- stack.h
+++ stack.h
@@ -4,23 +4,37 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <locale.h>
 #include <math.h>
 #include <assert.h>
 #include <stdbool.h>
 #include <iostream>
 #include <conio.h>
+#include "stack_config.h"
 
-struct node
+struct stack_t
 {
-    double data;
-    struct node* p;
+    int CANARY_LEFT;
+
+    double* data;
+    int capacity;//max size
+    int cur_size;//size now
+
+    int CANARY_RIGHT;
+
 };
 
-void push(node **pptop, double date);
+void stack_construct(int max_c, stack_t* Stack);
 
-double pop(node **pptop);
+void stack_destruct(stack_t* Stack);
 
-void print(node **pptop);
+void push_stack(stack_t* Stack, double push_num);
+
+double pop_stack(stack_t* Stack);
+
+void stack_dump(stack_t* Stack);
+
+int stack_verify(stack_t* Stack);
 
 
 #endif // STACK_H_INCLUDED
