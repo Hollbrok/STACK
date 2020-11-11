bac461d2368c7cf86d46d1dcbbb0c281affcf4a4
Talashkevich Daniil <70323332+Hollbrok@users.noreply.github.com>
2020-11-11 22:55:25 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,24 +7,38 @@ int main()
 
     double zero = 0;
 
-    stack_construct(8, &Stack);
+    stack_construct(5, &Stack);
+    //stack_construct(5, &Stack);
 
     push_stack(&Stack, 1);
+    stack_dump(&Stack);
+
     push_stack(&Stack, 4);
+    stack_dump(&Stack);
+
     push_stack(&Stack, 0/zero);
     push_stack(&Stack, 2.444);
     push_stack(&Stack, NAN);
     push_stack(&Stack, 666);
 
-    //stack_dump(&Stack);
+    stack_dump(&Stack);
 
-    //pop_stack(&Stack);
+    pop_stack(&Stack);
+    pop_stack(&Stack);
 
     stack_dump(&Stack);
 
     stack_destruct(&Stack);
 
+    /*FILE* finish = fopen("GOOD.txt", "a");
+    fprintf(finish, "GOOD");
+    fclose(finish);*/
 
+    stack_dump(&Stack);
+
+    FILE* finish = fopen("GOOD.txt", "w");
+    fprintf(finish, "GOOD");
+    fclose(finish);
 
     return 1337;
 }
