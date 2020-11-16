49768d8c0f7c4f7b807e118f33675991d450de57
Talashkevich Daniil <70323332+Hollbrok@users.noreply.github.com>
2020-11-16 23:24:48 +0300

    Update stack.h

--- stack.h
+++ stack.h
@@ -17,37 +17,45 @@
         return;                                         \
     }
 
-#define START_ACTIONS                                   \
-    setlocale(LC_ALL, "russian");                       \
-    stack_t Stack= {};                                  \
-    FILE* res = fopen("log_stack.txt", "w");            \
-    fclose(res);                                        \
-    double zero = 0;
+#define START_ACTIONS(a)                                \
+    stack_t a = {};                                     \
+
+#define Construct(a,b)                                  \
+        name = #a;                                      \
+        name++;                                         \
+        stack_construct(a, b, name);
+
 
 struct stack_t
 {
     int canary_left_stack;
+
     double* data;
+    char* name;
     int capacity;//max size
     int cur_size;//size now
+    int hash_stack;
+
     int canary_right_stack;
 
 };
 
-void stack_construct(int max_c, stack_t* Stack);
+void stack_construct(stack_t* Stack, int max_c, char* name);
 
 void stack_destruct(stack_t* Stack);
 
 void push_stack(stack_t* Stack, double push_num);
 
-double pop_stack(stack_t* Stack);
-
 void stack_dump(stack_t* Stack);
+
+void add_memory(stack_t* Stack);
+
+double pop_stack(stack_t* Stack);
 
 int stack_verify(stack_t* Stack);
 
-char* error_print(int ERROR_STATE);
+int hash_stack(stack_t* Stack);
 
-void add_memory(stack_t* Stack);
+char* error_print();
 
 #endif // STACK_H_INCLUDED
