b6df02eb054391af1e4dd5d93eb8fbfa28d271a6
Talashkevich Daniil <70323332+Hollbrok@users.noreply.github.com>
2020-11-19 01:40:28 +0300

    Update stack_config.h

--- stack_config.h
+++ stack_config.h
@@ -5,7 +5,11 @@ const int CANARY_LEFT_STACK  = 0x1A3C3D7C;
 const int CANARY_RIGHT_STACK = 0xADF44A23;
 const int CANARY_LEFT_DATA   = 0x1BF3334A;
 const int CANARY_RIGHT_DATA  = 0xFDF4223A;
+const int REAL_MULTIPLIER    = 2;
+const int REAL_REDUCER       = 2 * REAL_MULTIPLIER;
+const int REAL_ADDER         = 4;
 const double POISON          = NAN;
+
 
 
 enum
@@ -21,6 +25,7 @@ enum
     ERROR_DATA_RIGHT    = 10,
     ERROR_STACK_LEFT    = 11,
     ERROR_STACK_RIGHT   = 12,
+    HACK_STACK          = 13,
 };
 
 
