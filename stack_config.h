e47d880b4dab265ec4e485adc769a0cf033bc5a7
Talashkevich Daniil <70323332+Hollbrok@users.noreply.github.com>
2020-11-25 18:41:36 +0300

    Update stack_config.h

--- stack_config.h
+++ stack_config.h
@@ -8,8 +8,6 @@ const int CANARY_RIGHT_DATA  = 0xFDF4223A;
 const int REAL_MULTIPLIER    = 2;
 const int REAL_REDUCER       = 2 * REAL_MULTIPLIER;
 const int REAL_ADDER         = 4;
-const double POISON          = NAN;
-
 
 
 enum
