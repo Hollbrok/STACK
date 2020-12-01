328c307a55671317be9df3f838cb3e7969a9895a
Talashkevich Daniil <70323332+Hollbrok@users.noreply.github.com>
2020-12-01 16:15:18 +0300

    Update stack.h

--- stack.h
+++ stack.h
@@ -1,7 +1,6 @@
 #ifndef STACK_H_INCLUDED
 #define STACK_H_INCLUDED
 
-//#include <Txlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -12,68 +11,71 @@
 #include <limits.h>
 #include <typeinfo>
 #include <ctype.h>
-//#include <iostream>
-//#include <cmath>
-//#include <cfloat>
 #include "stack_config.h"
-#include "def_type.h"
+//#include "def_type.h"
+
+#define STRING_T
 
 #ifdef DOUBLE_T
     typedef double type_data;
-    const type_data POISON = NAN;
+    const type_data POISON             = NAN;
+    const type_data CANARY_LEFT_DATA   = 0x1BF3334A;
+    const type_data CANARY_RIGHT_DATA  = 0xFDF4223A;
+    const int CANARY_L_STACK           = 0x1A3C3D7C;
+    const int CANARY_R_STACK           = 0xADF44A23;
     #define FORMAT "lg"
-    const int CANARY_LEFT_DATA   = 0x1BF3334A;
-    const int CANARY_RIGHT_DATA  = 0xFDF4223A;
-    const int CANARY_LEFT_STACK  = 0x1A3C3D7C;
-    const int CANARY_RIGHT_STACK = 0xADF44A23;
     //const char* TYPE = "DOUBLE";
 #endif
 
 
 #ifdef INT_T
     typedef int type_data;
-    const type_data POISON = INT_MAX;
+    const type_data POISON             = INT_MAX;
+    const type_data CANARY_LEFT_DATA   = 0x1BF3334A;
+    const type_data CANARY_RIGHT_DATA  = 0xFDF4223A;
+    const int CANARY_L_STACK           = 0x1A3C3D7C;
+    const int CANARY_R_STACK           = 0xADF44A23;
     #define FORMAT "d"
-    const int CANARY_LEFT_DATA   = 0x1BF3334A;
-    const int CANARY_RIGHT_DATA  = 0xFDF4223A;
-    const int CANARY_LEFT_STACK  = 0x1A3C3D7C;
-    const int CANARY_RIGHT_STACK = 0xADF44A23;
     //const char* TYPE = "INT";
 #endif
 
 
 #ifdef FLOAT_T
     typedef float type_data;
-    const type_data POISON = NAN;
+    const type_data POISON             = NAN;
+    const type_data CANARY_LEFT_DATA   = 0x1BF3334A;
+    const type_data CANARY_RIGHT_DATA  = 0xFDF4223A;
+    const int CANARY_LEFT_STACK        = 0x1A3C3D7C;
+    const int CANARY_RIGHT_STACK       = 0xADF44A23;
     #define FORMAT "f"
-    const int CANARY_LEFT_DATA   = 0x1BF3334A;
-    const int CANARY_RIGHT_DATA  = 0xFDF4223A;
-    const int CANARY_LEFT_STACK  = 0x1A3C3D7C;
-    const int CANARY_RIGHT_STACK = 0xADF44A23;
     //const char* TYPE = "FLOAT";
 #endif
 
 #ifdef STRING_T
     typedef char* type_data;
-    const type_data POISON = nullptr;
+    const type_data POISON             = "LLLLLLLLLLL";
+    const type_data CANARY_LEFT_DATA   = "glavz";
+    const type_data CANARY_RIGHT_DATA  = "amoqc";
+    const int CANARY_L_STACK           = 0x1A3C3D7C;
+    const int CANARY_R_STACK           = 0xADF44A23;
     #define FORMAT "s"
-    const int CANARY_LEFT_DATA   = "iapvknka";
-    const int CANARY_RIGHT_DATA  = ".pudsdaf";
-    const char* CANARY_LEFT_STACK  = "fbagaaqq";
-    const char* CANARY_RIGHT_STACK = "adnnzziu";
     //const char* TYPE = "char*";
 #endif
 
 #ifdef CHAR_T
     typedef char type_data;
-    const type_data POISON = 'z';
+    const type_data POISON             = 'z';
+    const type_data CANARY_LEFT_DATA   = 's';
+    const type_data CANARY_RIGHT_DATA  = 'm';
+    const int CANARY_L_STACK           = 0x1A3C3D7C;
+    const int CANARY_R_STACK           = 0xADF44A23;
     #define FORMAT "c"
-    const char CANARY_LEFT_DATA   = 's';
-    const char CANARY_RIGHT_DATA  = 'm';
-    const int CANARY_LEFT_STACK  = 'i';
-    const int CANARY_RIGHT_STACK = 'h';
     //const char* TYPE = "char";
 #endif
+
+//#undef STRING_T
+
+
 
 struct stack_t
 {
@@ -98,12 +100,19 @@ struct stack_t
     }
 
 #define START_ACTIONS(a)                                \
-    stack_t a = {};                                     \
+    stack_t a = {};
 
 #define Construct(a,b)                                  \
         name = #a;                                      \
         name++;                                         \
         stack_construct(a, b, name);
+
+#define ASSERT_POP_OK                                   \
+    if (stack_verify(Stack))                            \
+    {                                                   \
+        stack_dump(Stack);                              \
+        return POISON;                                  \
+    }
 
 
 void stack_construct(stack_t* Stack, int max_c, char* name);
