d2fc05ac6bcb985a05e50e40ef1d0502066aa625
Talashkevich Daniil <70323332+Hollbrok@users.noreply.github.com>
2020-12-01 03:06:14 +0300

    Update stack_config.h

--- stack_config.h
+++ stack_config.h
@@ -1,15 +1,14 @@
 #ifndef STACK_CONFIG_H_INCLUDED
 #define STACK_CONFIG_H_INCLUDED
 
-const int CANARY_LEFT_STACK  = 0x1A3C3D7C;
-const int CANARY_RIGHT_STACK = 0xADF44A23;
-const int CANARY_LEFT_DATA   = 0x1BF3334A;
-const int CANARY_RIGHT_DATA  = 0xFDF4223A;
+//const int CANARY_LEFT_STACK  = 0x1A3C3D7C;
+//const int CANARY_RIGHT_STACK = 0xADF44A23;
+//const int CANARY_LEFT_DATA   = 0x1BF3334A;
+//const int CANARY_RIGHT_DATA  = 0xFDF4223A;
 const int REAL_MULTIPLIER    = 2;
 const int REAL_REDUCER       = 2 * REAL_MULTIPLIER;
 const int REAL_ADDER         = 4;
-const int TRUE               = 0;
-const int FLASE              = 0;
+const int POISON_SIZES       = NAN;
 
 
 enum
