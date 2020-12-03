c5843fa8142d4f5fcc4e90b40b9d7199c5dd62ee
Talashkevich Daniil <70323332+Hollbrok@users.noreply.github.com>
2020-12-04 01:58:41 +0300

    Update stack.h

--- stack.h
+++ stack.h
@@ -12,9 +12,6 @@
 #include <typeinfo>
 #include <ctype.h>
 #include "stack_config.h"
-//#include "def_type.h"
-
-//#define STRING_T
 
 #ifdef DOUBLE_T
     typedef double type_data;
@@ -24,7 +21,6 @@
     const int CANARY_L_STACK           = 0x1A3C3D7C;
     const int CANARY_R_STACK           = 0xADF44A23;
     #define FORMAT "lg"
-    //const char* TYPE = "DOUBLE";
 #endif
 
 
@@ -36,7 +32,6 @@
     const int CANARY_L_STACK           = 0x1A3C3D7C;
     const int CANARY_R_STACK           = 0xADF44A23;
     #define FORMAT "d"
-    //const char* TYPE = "INT";
 #endif
 
 
@@ -48,7 +43,6 @@
     const int CANARY_LEFT_STACK        = 0x1A3C3D7C;
     const int CANARY_RIGHT_STACK       = 0xADF44A23;
     #define FORMAT "f"
-    //const char* TYPE = "FLOAT";
 #endif
 
 #ifdef STRING_T
@@ -59,7 +53,6 @@
     const int CANARY_L_STACK           = 0x1A3C3D7C;
     const int CANARY_R_STACK           = 0xADF44A23;
     #define FORMAT "s"
-    //const char* TYPE = "char*";
 #endif
 
 #ifdef CHAR_T
@@ -70,28 +63,24 @@
     const int CANARY_L_STACK           = 0x1A3C3D7C;
     const int CANARY_R_STACK           = 0xADF44A23;
     #define FORMAT "c"
-    //const char* TYPE = "char";
 #endif
 
-#ifdef LOW_SECURE // только проверка на Stack == nullptr, stack_construct
+#ifdef LOW_SECURE       // только проверка на Stack == nullptr, stack_construct
     const int low_sec   = 1;
     const int med_sec   = 0;
     const int high_sec  = 0;
-    //const char* sec_lvl = "LOW";
 #endif LOW_SECURE
 
-#ifdef MEDIUM_SECURE // все проверки, кроме хэширования и канареек
+#ifdef MEDIUM_SECURE    // все проверки, кроме хэширования и канареек
     const int low_sec   = 0;
     const int med_sec   = 1;
     const int high_sec  = 0;
-   // const char* sec_lvl = "MEDIUM";
 #endif MEDIUM_SECURE
 
-#ifdef HIGH_SECURE // все проверки
+#ifdef HIGH_SECURE      // все проверки
     const int low_sec   = 0;
     const int med_sec   = 0;
     const int high_sec  = 1;
-    //const char* sec_lvl = "HIGH";
 #endif HIGH_SECURE
 
 
@@ -101,8 +90,8 @@ struct stack_t
 
     type_data* data       = nullptr;
     char* name            = nullptr;
-    int capacity          ;//max size
-    int cur_size          ;//size now
+    int capacity          ;         //max size
+    int cur_size          ;         //size now
     int hash_stack        ;
 
     int canary_right_stack;
@@ -153,4 +142,4 @@ char* error_print();
 
 char* define_lvl();
 
-#endif // STACK_H_INCLUDED
+#endif
