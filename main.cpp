f38040c4fb9788855616b083b3942613abf01512
Talashkevich Daniil <70323332+Hollbrok@users.noreply.github.com>
2020-12-04 02:29:37 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -20,30 +20,21 @@ int main()
     START_ACTIONS(Stack);
 
     Construct(&Stack, 1);
-    //printf("00");
     stack_dump(&Stack);
-    //printf("\nafter 1 dump\n");
 
     push_stack(&Stack, "faa");
-    //printf("after 1 push\n");
-
     stack_dump(&Stack);
 
     push_stack(&Stack, "gaa");
     stack_dump(&Stack);
 
-    //Stack.data[2] = 24;// hack of the stack
-
     pop_stack(&Stack);
     pop_stack(&Stack);
     pop_stack(&Stack);
     pop_stack(&Stack);
-    //pop_stack(&Stack);
-    //pop_stack(&Stack);
     stack_dump(&Stack);
 
     stack_destruct(&Stack);
-    //stack_destruct(&Stack);
 
     START_ACTIONS(Stack2);
     Construct(&Stack2, 5);
