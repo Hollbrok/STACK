01059375d0a8343ac76a09404ec0a94f9a183d29
Talashkevich Daniil <70323332+Hollbrok@users.noreply.github.com>
2020-12-04 22:12:54 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,29 +11,32 @@ int main()
 
     START_ACTIONS(Stack);
 
-    Construct(&Stack, 1);
+    Construct(&Stack, 2);
+    //stack_dump(&Stack);
+
+    push_stack(&Stack, "DAROVA");
+    push_stack(&Stack, "this is ");
     stack_dump(&Stack);
 
-    push_stack(&Stack, "faa");
-    stack_dump(&Stack);
-
-    push_stack(&Stack, "gaa");
+    push_stack(&Stack, "my stack");
     stack_dump(&Stack);
 
     pop_stack(&Stack);
     pop_stack(&Stack);
-    pop_stack(&Stack);
-    pop_stack(&Stack);
+
     stack_dump(&Stack);
 
     stack_destruct(&Stack);
 
     START_ACTIONS(Stack2);
-    Construct(&Stack2, 5);
+    Construct(&Stack2, 1);
 
     stack_dump(&Stack2);
 
-    push_stack(&Stack2, "jksbz");
+    push_stack(&Stack2, "This is the 2nd stack");
+    push_stack(&Stack2, "with the other name");
+    push_stack(&Stack2, "ANIME");
+    push_stack(&Stack2, "Goodbye");
     stack_dump(&Stack2);
 
     //printf("FUCKING INSANE");
